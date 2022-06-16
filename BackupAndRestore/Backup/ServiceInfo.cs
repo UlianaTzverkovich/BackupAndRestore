0f5ee5aca366708c47f9cb5402fb5a356fa2874a
Uliana <74460616+UlianaTzverkovich@users.noreply.github.com>
2022-06-16 10:10:12 +0300

    delete

--- BackupAndRestore/Backup/ServiceInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using Library;
-
-namespace Backup;
-public class ServiceInfo : GeneralBackupParameters
-{
-    public string SearchMask { get; set; } = string.Empty;
-    public ServiceInfo() { }
-}
